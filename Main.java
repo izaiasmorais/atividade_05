360161a51602b84d3cfced1589f75c211945579d
Izaias Lima <izaiaslima356@gmail.com>
2025-05-04 10:16:59 -0300

    feat: espaçamento na mensagem na classe main formatado. closes #1

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		System.out.println(mainMenu.getSelection() + " foi selecionada");
 		System.out.println("Fim");
 	}
 
