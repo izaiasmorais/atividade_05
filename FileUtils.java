9382dc2cd9778ebb822956facb1c352dc84689a8
Jailson Zarur <jailsoncardoz123@gmail.com>
2025-05-05 09:28:53 -0300

    feat: adição da funcionalidade de editar cliente por cpf no arquivo. #13

--- FileUtils.java
+++ FileUtils.java
@@ -44,4 +44,34 @@ public class FileUtils {
 
 		return encontrado;
 	}
+
+	public static boolean editarClientePorCpf(String caminho, String cpf, String novoNome) throws IOException {
+		File inputFile = new File(caminho);
+		File tempFile = new File("temp.txt");
+
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+		String currentLine;
+		boolean encontrado = false;
+
+		while ((currentLine = reader.readLine()) != null) {
+			if (currentLine.contains(cpf)) {
+				encontrado = true;
+				String novaLinha = "Cliente{nome='" + novoNome + "', cpf='" + cpf + "'}";
+				writer.write(novaLinha + System.lineSeparator());
+			} else {
+				writer.write(currentLine + System.lineSeparator());
+			}
+		}
+
+		writer.close();
+		reader.close();
+
+		if (!inputFile.delete()) return false;
+		if (!tempFile.renameTo(inputFile)) return false;
+
+		return encontrado;
+	}
+
 }
